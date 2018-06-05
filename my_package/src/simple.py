917c13964c542b975ed6e19c061a06e7ee186efa
bobjuventus <longyu.zhao.ustc@gmail.com>
2018-06-04 23:58:43 -0700

    This is the github test setup on xiaohei.

--- my_package/src/simple.py
+++ my_package/src/simple.py
@@ -5,5 +5,5 @@ import rospy
 rospy.init_node("ObiWang")
 rate = rospy.Rate(2)               # We create a Rate object of 2Hz
 while not rospy.is_shutdown():     # Endless loop until Ctrl + C
-   print "Help me Obi-Wan Kenobi, you're my only hope"
+   print "Help me Obi-Wan Kenobi, you're my only hope. Test!!"
    rate.sleep()
